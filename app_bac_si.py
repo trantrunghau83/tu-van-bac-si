367a4c17ae258b0116ed72cb33e28b798503b3f6
trantrunghau83 <trantrunghau@thcsthuanhung.edu.vn>
2026-04-26 18:52:57 +0700

    Refactor app_bac_si.py for improved functionality

--- app_bac_si.py
+++ app_bac_si.py
@@ -55,7 +55,7 @@ st.markdown("""
     
     /* Đảm bảo chữ trong tab cũng rõ ràng */
     .stTabs [data-baseweb="tab"] p {
-        color: #2E5A56 !important;
+        color: #8B0000 !important;
         font-weight: bold;
     }
     </style>
